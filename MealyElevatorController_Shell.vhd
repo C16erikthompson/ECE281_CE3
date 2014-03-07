299b3af3cc2ca1b8874dbf171fc4eee45fda4f7e
Erik Thompson <C16erik.thompson@usafa.edu>
2014-03-07 14:48:31 -0700

    fifth (so late...)

--- MealyElevatorController_Shell.vhd
+++ MealyElevatorController_Shell.vhd
@@ -117,15 +117,15 @@ end process;
 --Code your Ouput Logic for your Mealy machine below
 --Remember, now you have 2 outputs (floor and nextfloor)
 -----------------------------------------------------------
-floor <= "0001" when ((floor_state = floor1) and (stop = '1')) or ((floor_state = floor1) and (up_down = '0' and stop = '0')) or (((floor_state = floor2) and (up_down = '0' and stop = '0')) and (clk'event and clk='1')) else
-			"0010" when ((floor_state = floor2) and (stop = '1')) or ((((floor_state = floor1) and (up_down = '1' and stop = '0')) or ((floor_state = floor3) and (up_down = '0' and stop = '0'))) and (clk'event and clk='1'))else
-			"0011" when ((floor_state = floor3) and (stop = '1')) or ((((floor_state = floor2) and (up_down = '1' and stop = '0')) or ((floor_state = floor4) and (up_down = '0' and stop = '0'))) and (clk'event and clk='1'))else
-			"0100" when ((floor_state = floor4) and (stop = '1')) or ((floor_state = floor4) and (up_down = '1' and stop = '0')) or (((floor_state = floor3) and (up_down = '1' and stop = '0')) and (clk'event and clk='1'));
+floor <= "0001" when (floor_state = floor1) else
+			"0010" when (floor_state = floor2) else
+			"0011" when (floor_state = floor3) else
+			"0100" when (floor_state = floor4) ;
 			
-nextfloor <= "0001" when ((floor_state = floor2) and (up_down = '0' and stop = '0'))else
-				 "0010" when ((floor_state = floor3) and (up_down = '0' and stop = '0')) or floor_state = floor1 else 
-				 "0011" when floor_state = floor4 or ((floor_state = floor2) and (up_down = '1' and stop = '0')) else
-				 "0100" when ((floor_state = floor3) and (up_down = '1' and stop = '0')); 
+nextfloor <= "0001" when ((floor_state = floor2) and (up_down = '0' and stop = '0')) or ((floor_state = floor1) and (up_down = '0' and stop = '0')) or ((floor_state = floor1) and stop = '1')else
+				 "0010" when ((floor_state = floor3) and (up_down = '0' and stop = '0')) or ((floor_state = floor1) and (up_down = '1' and stop = '0')) or ((floor_state = floor2) and stop = '1') else 
+				 "0011" when ((floor_state = floor4) and (up_down = '0' and stop = '0')) or ((floor_state = floor2) and (up_down = '1' and stop = '0')) or ((floor_state = floor3) and stop = '1') else
+				 "0100" when ((floor_state = floor4) and (up_down = '1' and stop = '0')) or ((floor_state = floor3) and (up_down = '1' and stop = '0')) or ((floor_state = floor4) and stop = '1'); 
 
 
 end Behavioral;
